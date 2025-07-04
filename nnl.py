252b271b6cc8b45609695c9d0c16de61cb53314d
Maiths <maithscvl@gmail.com>
2025-07-04 16:50:25 +0200

    improving my code

--- nnl.py
+++ nnl.py
@@ -2,19 +2,19 @@ import numpy as np
 import sys
 
 def Sigmoid(x, obj, deriv=False):
-            if deriv==True:
-                s = Sigmoid(x, obj)
-                return s * (1-s)
-            return 1 / (1+np.exp(-x))
+    if deriv==True:
+        s = Sigmoid(x, obj)
+        return s * (1-s)
+    return 1 / (1+np.exp(-x))
         
 def ReLU(x, deriv=False):
-            if deriv==True:
-                return (x > 0).astype(float)
-            return np.maximum(0, x)
+    if deriv==True:
+        return (x > 0).astype(float)
+    return np.maximum(0, x)
 
 class NeuralNetwork:
 
-    def __init__(self, layers, activation="sigmoid", last_layer_activation="sigmoid", npRandomSeed=0): #layers : [input, hidden, ..., hidden, output]
+    def __init__(self, layers, activation="sigmoid", last_layer_activation="sigmoid", npRandomSeed=0):
         np.random.seed(npRandomSeed)
         self.weights = {}
         self.biases = {}
@@ -32,44 +32,64 @@ class NeuralNetwork:
             self.biases[i+1] = np.random.randn(1, self.layers[i+1]) * 0.1
 
     def activFunction(self, x, deriv=False):
-          if self.activation=="sigmoid":
-                return Sigmoid(x, self, deriv)
-          elif self.activation=="relu":
-                return ReLU(x, deriv)
-          else:
-                print("There was an error in forward activation function")
-                sys.exit()
+        if self.activation=="sigmoid":
+            return Sigmoid(x, self, deriv)
+        elif self.activation=="relu":
+            return ReLU(x, deriv)
+        else:
+            print("There was an error in forward activation function")
+            sys.exit()
         
     def lastLayerActivFunction(self, x, deriv=False):
-          if self.last_layer_activation=="sigmoid":
-                return Sigmoid(x, self, deriv)
-          elif self.last_layer_activation=="relu":
-                return ReLU(x, deriv)
-          else:
-                print("There was an error in forward activation function")
-                sys.exit()
+        if self.last_layer_activation=="sigmoid":
+            return Sigmoid(x, self, deriv)
+        elif self.last_layer_activation=="relu":
+            return ReLU(x, deriv)
+        else:
+            print("There was an error in forward activation function")
+            sys.exit()
           
 
     def forward(self, x):
-            self.output[1] = x
-            for i in range(len(self.layers)-1): # i : 0, 1, first layer then 2nd layer => output 2 then output 3
-                self.z[i+2] = x @ self.weights[i+1].T + self.biases[i+1]
-                if len(self.layers)-2==i: # 1 = last i, propagation
-                    self.output[i+2] = self.lastLayerActivFunction(self.z[i+2])
-                else:
-                    self.output[i+2] = self.activFunction(self.z[i+2])
-                x = self.output[i+2]
+        self.output[1] = x
+        for i in range(len(self.layers)-1):
+            self.z[i+2] = x @ self.weights[i+1].T + self.biases[i+1]
+            if len(self.layers)-2==i:
+                self.output[i+2] = self.lastLayerActivFunction(self.z[i+2])
+            else:
+                self.output[i+2] = self.activFunction(self.z[i+2])
+            x = self.output[i+2]
 
     def backward(self, loss, learningRate):
-            for o in reversed(range(2, len(self.layers)+1)): #i: 0, 1
-                if len(self.layers)==o: # 3 = first o, backpropagation 
-                    delta = loss * self.lastLayerActivFunction(self.z[o], deriv=True)
-                else:
-                    delta = self.back * self.activFunction(self.z[o], deriv=True)
+        for o in reversed(range(2, len(self.layers)+1)):
+            if len(self.layers)==o:
+                delta = loss * self.lastLayerActivFunction(self.z[o], deriv=True)
+            else:
+                delta = self.back * self.activFunction(self.z[o], deriv=True)
 
-                dw = delta.T @ self.output[o-1]
-                db = np.sum(delta, axis=0, keepdims=True)
-                self.weights[o-1] -= learningRate * dw
-                self.biases[o-1] -= learningRate * db
+            dw = delta.T @ self.output[o-1]
+            db = np.sum(delta, axis=0, keepdims=True)
+            self.weights[o-1] -= learningRate * dw
+            self.biases[o-1] -= learningRate * db
 
-                self.back = delta @ self.weights[o-1]
+            self.back = delta @ self.weights[o-1]
+        
+    def learnAlgo(self, x, learningrate, error):
+        self.forward(x)
+        self.backward(error, learningrate)
+
+    def learn(self, x, learningrate, error, precision, repeat, errorNd):
+        if precision=="none" and repeat==0:
+            print("Please enter a precision to reach of amount of times to repeat")
+            sys.exit()
+
+        elif precision=="none":
+            for i in range(repeat):
+                self.learnAlgo(x, learningrate, error)
+
+        else:
+            while errorNd > (1-precision/100)**2:
+                self.learnAlgo()
+
+        
+
